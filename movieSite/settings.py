eb7ccc80eaee42158d9d4b758bdd916516692fee
unknown <antonydamico29@gmail.com>
2018-09-05 12:18:24 -0400

    Hiding the SECRET_KEY

--- movieSite/settings.py
+++ movieSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hmu#4i%s=9)+yc2=64ei&bbw3nhg+d=4qehno36sy)&&ianpdw'
+SECRET_KEY = PERSONAL_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ secrets.py
@@ -0,0 +1,2 @@
+# this file should be in .gitignore but since this is a demo I'll keep it here
+PERSONAL_SECRET_KEY = 'hmu#4i%s=9)+yc2=64ei&bbw3nhg+d=4qehno36sy)&&ianpdw'

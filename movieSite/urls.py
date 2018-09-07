5ee4d04f81124f0e4a8495797ef5e4b7f1a37f0b
unknown <antonydamico29@gmail.com>
2018-09-06 22:10:34 -0400

    adding the movielist to the landing page

--- movieSite/urls.py
+++ movieSite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import movieList.views
 
 urlpatterns = [
+    path('', movieList.views.IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('movieList/', include('movieList.urls'))
 ]
